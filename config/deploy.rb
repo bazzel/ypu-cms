430c5e93dda84a114d7f897fe96d1782c3c77ad0
Patrick Baselier <patrick@kabisa.nl>
2012-10-30 19:26:52 +0100

    Remove line not needed by rvm-capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,5 @@
 # RVM bootstrap
 set :rvm_ruby_string, '1.9.3-p194@ypu-cms'
-$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
 require "rvm/capistrano"                               # Load RVM's capistrano plugin.
 
 # When using whenever gem:
