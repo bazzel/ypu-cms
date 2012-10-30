dfc862de018b06a98cf63781ac60dafb64a89990
Patrick Baselier <patrick@kabisa.nl>
2012-10-30 19:08:51 +0100

    Clean cms.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YpuCms::Application.config.secret_token = '6aa070e18561dae44b41f1677acaf7ae1c646776043401e36220102f2db24c161db8a58e75c88410fea817beb5c32124ad65310178d58f3205406fdcbe1f6d46'
