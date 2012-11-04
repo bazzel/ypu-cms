86f223a2fbb02fbd59b4c81da2f9ee73e28b522e
Patrick Baselier <patrick@kabisa.nl>
2012-11-04 12:34:26 +0100

    Setup i18n

--- config/initializers/refinery/i18n.rb
+++ config/initializers/refinery/i18n.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 Refinery::I18n.configure do |config|
-  # config.enabled = true
+   config.enabled = true
 
   # config.default_locale = :en
 
@@ -9,7 +9,7 @@ Refinery::I18n.configure do |config|
 
   # config.default_frontend_locale = :en
 
-  # config.frontend_locales = [:en]
+   config.frontend_locales = [:en, :nl, :de]
 
-  # config.locales = {:en=>"English", :fr=>"Français", :nl=>"Nederlands", :"pt-BR"=>"Português", :da=>"Dansk", :nb=>"Norsk Bokmål", :sl=>"Slovenian", :es=>"Español", :it=>"Italiano", :de=>"Deutsch", :lv=>"Latviski", :ru=>"Русский", :sv=>"Svenska", :pl=>"Polski", :"zh-CN"=>"Simplified Chinese", :"zh-TW"=>"Traditional Chinese", :el=>"Ελληνικά", :rs=>"Srpski", :cs=>"Česky", :sk=>"Slovenský", :ja=>"日本語", :bg=>"Български"}
+   config.locales = {:en=>"English", :fr=>"Français", :nl=>"Nederlands", :"pt-BR"=>"Português", :da=>"Dansk", :nb=>"Norsk Bokmål", :sl=>"Slovenian", :es=>"Español", :it=>"Italiano", :de=>"Deutsch", :lv=>"Latviski", :ru=>"Русский", :sv=>"Svenska", :pl=>"Polski", :"zh-CN"=>"Simplified Chinese", :"zh-TW"=>"Traditional Chinese", :el=>"Ελληνικά", :rs=>"Srpski", :cs=>"Česky", :sk=>"Slovenský", :ja=>"日本語", :bg=>"Български"}
 end
